e7729ccc7dbde367aae401e6ac83069d2ba1a425
MD. MONIR HOSSIN <abccom272@gmail.com>
2025-02-16 22:05:28 +0600

    input tk added to the display on barishal flood

--- js/utility.js
+++ js/utility.js
@@ -2,7 +2,10 @@ document.getElementById('barishal-flood-btn').addEventListener('click', function
     const donationTaka = document.getElementById('barishal-flood-tk').value;
     const numberOfTaka = parseFloat(donationTaka);
     if (!isNaN(numberOfTaka)) {
-        console.log('The value is correct');
+        let displayTakaElement = document.getElementById('barishal-flood-tk-display');
+        let displayTkNumber = parseFloat(displayTakaElement.innerText);
+        const newDonation = displayTkNumber + numberOfTaka;
+        displayTakaElement.innerText = newDonation;
     }
     else {
         alert("Please write a number only");
