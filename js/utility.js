216010c2b1446cbe542b6abea2b2200e444f0b11
MD. MONIR HOSSIN <abccom272@gmail.com>
2025-02-18 13:46:43 +0600

    add and substract from main balance

--- js/utility.js
+++ js/utility.js
@@ -1,11 +1,20 @@
 document.getElementById('barishal-flood-btn').addEventListener('click', function () {
-    const donationTaka = document.getElementById('barishal-flood-tk').value;
+    const donationInput = document.getElementById('barishal-flood-tk');
+    const donationTaka = donationInput.value;
     const numberOfTaka = parseFloat(donationTaka);
     if (!isNaN(numberOfTaka)) {
         let displayTakaElement = document.getElementById('barishal-flood-tk-display');
         let displayTkNumber = parseFloat(displayTakaElement.innerText);
         const newDonation = displayTkNumber + numberOfTaka;
         displayTakaElement.innerText = newDonation;
+
+
+        let mainBalanceDisplay = document.getElementById('main-balance');
+        let mainBalanceNumber = parseFloat(mainBalanceDisplay.innerText);
+        const newMainBalance = mainBalanceNumber - numberOfTaka;
+        mainBalanceDisplay.innerText = newMainBalance;
+
+        donationInput.value = "";
     }
     else {
         alert("Please write a number only");
